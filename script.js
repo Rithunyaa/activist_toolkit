87c6700961ac4c61c6e9c00e9a8426a74f00ae4d
BlaBlahBlahh <rithunyaa@gmail.com>
2026-08-08 16:52:49 +0000

    added basic placeholder code

---
+++ script.js
@@ -0,0 +1,15 @@
+const facts = [
+"FACT 1 PLACEHOLDER: Add a fact about light pollution.",
+"FACT 2 PLACEHOLDER: Add a fact about light pollution.",
+"FACT 3 PLACEHOLDER: Add a fact about light pollution.",
+"FACT 4 PLACEHOLDER: Add a fact about light pollution."
+];
+
+function generateFact() {
+const randomIndex = Math.floor(Math.random() * facts.length);
+
+```
+document.getElementById("fact").textContent = facts[randomIndex];
+```
+
+}
